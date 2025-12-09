919d25bb5d0c9dc3deab8f431d65bac9e20078ef
Dharma S <skdharma05@gmail.com>
2025-12-09 21:35:59 +0530

    Quiz Game : Final Update : All functionality Added

--- Quiz Game/index.js
+++ Quiz Game/index.js
@@ -63,3 +63,120 @@ const quizQuestions = [
   },
 ];
 
+// Quiz State Vars
+
+
+let currentQuestionIndex =0;
+let score =0;
+let answersDisable = false;
+
+totalQuestionsSpan.textContent=quizQuestions.length;
+maxScoreSpan.textContent=quizQuestions.length;
+
+
+// Event Listeners
+startButton.addEventListener("click",startQuiz);
+restartButton.addEventListener("click",restartQuiz);
+
+
+function startQuiz(){
+  currentQuestionIndex=0;
+  score =0;
+  scoreSpan.textContent=0;
+
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
+}
+
+function showQuestion() {
+  //reset state
+  answersDisable=false;
+
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+
+  currentQuestionSpan.textContent = currentQuestionIndex + 1;
+
+  const progressPercent = (currentQuestionIndex/quizQuestions.length)*100;
+  progressBar.style.width = progressPercent+"%";
+
+  questionText.textContent = currentQuestion.question;
+
+  answersContainer.innerHTML="";
+
+  currentQuestion.answers.forEach((answer)=>{
+    const button = document.createElement("button")
+    button.textContent = answer.text;
+    button.classList.add("answer-btn");
+
+    // what is dataset? its a property of the button element that allows you to store custom data.
+    button.dataset.correct=answer.correct;
+
+    button.addEventListener("click",selectAnswer);
+    answersContainer.appendChild(button);
+
+  });
+}
+
+
+function selectAnswer(event) {
+  // Optimization check 
+  if(answersDisable)return;
+
+  answersDisable=true;
+
+  const selectedButton = event.target;
+  const isCorrect = selectedButton.dataset.correct ==="true";
+
+  // Here Array.from() is used to convert the NodeList returned by answersContainer.children into an array, this is because the NodeList is not an array and we need to use the forEach method.
+  Array.from(answersContainer.children).forEach((button)=>{
+    if(button.dataset.correct ==="true"){
+      button.classList.add("correct");
+    }else if(button === selectedButton){
+      button.classList.add("incorrect");
+    }
+  });
+
+  if(isCorrect){
+    score++;
+    scoreSpan.textContent = score;
+
+  }
+  setTimeout(()=>{
+    currentQuestionIndex++;
+
+    // check if there are  more questions or if the quiz is over. 
+    if(currentQuestionIndex < quizQuestions.length){
+      showQuestion();
+    }else{
+      showResult()
+    }
+  },1000);
+}
+
+function showResult(){
+  quizScreen.classList.remove("active");
+  resultScreen.classList.add("active");
+
+  finalScoreSpan.textContent = score;
+  const percentage = (score/quizQuestions.length)*100;
+
+  if (percentage === 100) {
+    resultMessage.textContent = "Perfect! You're a genius!";
+  } else if (percentage >= 80) {
+    resultMessage.textContent = "Great job! You know your stuff!";
+  } else if (percentage >= 60) {
+    resultMessage.textContent = "Good effort! Keep learning!";
+  } else if (percentage >= 40) {
+    resultMessage.textContent = "Not bad! Try again to improve!";
+  } else {
+    resultMessage.textContent = "Keep studying! You'll get better!";
+  }
+
+}
+
+function restartQuiz() {
+resultScreen.classList.remove("active");
+startQuiz();
+}
